a25cc0c9873ac57f121e495b46c2400df3af50c8
mantanterindah <tenuz99@gmail.com>
2017-11-22 05:58:18 +0700

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,58 @@
+const botsettings = require('./botsettings.json');
+const Discord = require('discord.js');
+const request = require('request');
+const cheerio = require('cheerio');
+const snekfetch = require('snekfetch');
+const prefix = botsettings.prefix;
+const fs = require('fs')
+
+
+const bot = new Discord.Client();
+bot.commands = new Discord.Collection();
+
+fs.readdir("./cmds/", (err, files) => {
+	if(err) console.log(err);
+
+	let jsfiles = files.filter(f => f.split(".").pop() === "js");
+	if(jsfiles.length <= 0) {
+		console.log("Bingung mw Load Apaan gw??");
+		return;
+	}
+
+	console.log(`Loading ${jsfiles.length} commands!`)
+
+	jsfiles.forEach((f, i) => {
+		let props = require(`./cmds/${f}`);
+		console.log(`${i + 1}: ${f} Loaded!`);
+		bot.commands.set(props.help.name, props);
+	})
+});
+
+bot.on('ready', async () => {
+	console.log('Bot Ready!')
+	bot.commands.set(bot.command);
+});
+
+bot.on('message', message => {
+	console.log(`${message.author.tag}: ${message.content} at ${message.createdTimestamp}`)
+});
+
+bot.on('message', async message => {
+	if(message.author.bot) return;
+	if(message.channel.type === "dm") return;
+
+	let messageArray = message.content.split(/\s+/g);
+	let command = messageArray[0];
+	let args = messageArray.slice(1);
+
+
+
+	//if(!commands.startsWith(prefix)) return;
+
+	let cmd = bot.commands.get(command.slice(prefix.length));
+	if(cmd) cmd.run(bot, message, args);
+
+	
+})
+
+bot.login(botsettings.token)
