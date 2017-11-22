50cb8924e7ddaaa061e11ffd389774c7fc568738
mantanterindah <tenuz99@gmail.com>
2017-11-23 05:45:35 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -55,15 +55,6 @@ bot.on('message', async message => {
 
 	
 });
-bot.on('message', async message => {
-	if (message.content.startsWith(prefix + 'setstatus')) {
-		bot.user.setStatus(args);
-	} else
-	if (message.content.startsWith(prefix + 'setgame')) {
-		bot.user.setGame(args);
-	}
-});
-
 
 process.on('unhandledRejection', error => {
   console.error(`Uncaught Promise Error: \n${error.stack}`);
