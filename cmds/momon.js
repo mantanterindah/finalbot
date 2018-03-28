db2b9d8ab1bf44032d3c3640946c142ee625a48f
mantanterindah <tenuz99@gmail.com>
2018-03-28 20:25:34 +0700

    Update momon.js

--- cmds/momon.js
+++ cmds/momon.js
@@ -34,8 +34,8 @@ module.exports.run = async (bot, message, args) => {
 						.setTitle(momonnya)
 						.setColor(0x00AE86)
 						.setThumbnail(gambar)
-						.addField('**Drop List:**', drop1)
-						.addField('**Spawn Monsters**', "checking"+lokasi1);
+						.addField('**Drop List:**', "Checking:...."+'\n'+'**'+drop1+'**')
+						.addField('**Spawn Monsters**', "Checking:..."+'\n'+"```"+"js"+'\n'+lokasi1+"```");
 
 						message.channel.send(embed1);
 
