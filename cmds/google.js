a3a1f85307d826a78c8202b68f0d2473a05e8443
mantanterindah <tenuz99@gmail.com>
2018-04-28 09:53:32 +0700

    Add files via upload

---
+++ cmds/google.js
@@ -0,0 +1,45 @@
+const Discord = module.require('discord.js');
+const cheerio = module.require('cheerio');
+const request = module.require('request');
+
+module.exports.run = async (bot, message, args) => {
+  
+      let msg = await message.reply('Wait a Sec ...')
+
+      request(`https://www.google.com/search?q=${args}`, function(err, resp, body) {
+        if (err) {
+          console.log(err)
+        }
+
+        let $ = cheerio.load(body);
+
+            $('div.g').each(function(index) {
+                var result1 = $(this).children();
+                var title = result1.find('h3 a').text();
+                var url = result1.find('div cite').text();
+                var text = result1.find('div > span').text();
+
+                console.log(url+'\n'+text);
+
+                let embed = new Discord.RichEmbed()
+                  .setTitle(title)
+                  .setURL(url)
+                  .setColor('#e0d31a')
+                  .addField('Sub Text: ', text)
+                  .setThumbnail(message.author.avatarURL)
+                  .setTimestamp();
+
+                  message.channel.send(embed)
+
+          })
+      })
+
+
+}
+	
+	
+
+
+module.exports.help = {
+	name: "google"
+}
