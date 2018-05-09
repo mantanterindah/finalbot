a61b88a36761a12c0a72996d6c80c9449a303b84
mantanterindah <tenuz99@gmail.com>
2018-05-10 02:34:42 +0700

    Update ping.js

--- cmds/ping.js
+++ cmds/ping.js
@@ -7,7 +7,7 @@ module.exports.run = async (bot, message, args) => {
 	var fake = new Date();
 	var time = `${fake.getHours()} ${fake.getMinutes()} ${fake.getSeconds()} at ${fake.getDate()} ${fake.getMonth() +1} ${fake.getFullYear()}`
 	var msg2 = `${message.author.username} ${message.content} ${time}`+'\r\n'
-	fs.appendFile('report.txt', msg2, function(err) {
+	fs.appendFile('report.json', msg2, function(err) {
 		if (err) throw err;
 	})
 
