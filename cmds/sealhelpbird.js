5581e680dc69254d1237dd48b33b989dda913250
mantanterindah <tenuz99@gmail.com>
2019-03-12 03:02:55 +0700

    Add files via upload

---
+++ cmds/sealhelpbird.js
@@ -0,0 +1,265 @@
+const Discord = module.require('discord.js');
+
+module.exports.run = async (bot, message, args) => {
+
+	let msg = await message.reply('```diff\nKetik Angkanya Gajah ehh... Angkanya Maksudnya\n- 1. Bird Egg\n- 2. Legged Egg\n- 3. Bat Wings Egg\n- 4. Feather Wings Egg\n- 5.Baby Bat\n- 6.Blood Sucking\n- 7. Pig Bat\n- 8.Salamander\n- 9. Crow\n- 10. Pair Love\n- 11. Owl\n- 12. Eagle')
+
+	//let embed = new Discord.RichEmbed()
+	//	.setTitle('')
+	//	.setImage('')
+	//	.setColor('#320ddb')
+	//	.setThumbnail(bot.user.avatarURL)
+	//	.setTimestamp()
+		//.addField('', '')
+		//.addField('', '')
+	//	.addField('Bahan :', '');
+		//message.channel.send();
+
+	let birdeggtolegged = new Discord.RichEmbed()
+		.setTitle('Bird Egg ke Legged')
+		.setImage('https://cdn.discordapp.com/attachments/502501525529034762/554647175393574915/birdeggtolegged.png')
+		.setColor('#320ddb')
+		.setThumbnail('https://cdn.discordapp.com/attachments/502501525529034762/554753810338283540/unknown.png')
+		.setTimestamp()
+		//.addField('', '')
+		//.addField('', '')
+		.addField('Bahan :', '4 Leaf Clover\nHatching Stone\nGeranium\nLarge Geranium\nSap\nMilk\nBabys Pacifier\nMark of Wind\nAvas Wings\nMagic Powder\nMothers Heart\nMotherly Love\nMotherly Devotion\nFathers Excuse\nFathers Good Old Days\nCrows Feather\nChicken Feather\nMadams Feather\nWinged Shield\nBats Wings');
+		//message.channel.send();
+	
+	///Stage 2////////////////
+	let leggedtobwe = new Discord.RichEmbed()
+		.setTitle('Legged to Bat Wings Egg')
+		.setImage('https://cdn.discordapp.com/attachments/502501525529034762/554647274068901889/leggedtobwe.png')
+		.setColor('#320ddb')
+		.setThumbnail('https://cdn.discordapp.com/attachments/502501525529034762/554753878462038078/unknown.png')
+		.setTimestamp()
+		//.addField('', '')
+		//.addField('', '')
+		.addField('Bahan :', '4 Leaf Clover\nBats Wings\nBats Teeth\nVampires Canine\nTooth\nBlack Sap\nRotten Leaf\nPoisonous Claws\nMark of Vampire\nSteel\nStone\nRock\nIron Plate\nGasoline\nIron\nDevil Tail\nBat Guy Mask\nTarantulas Sunglasses\nForging Rock\nVolcanic Rock\nSeaweed\nMushroom Poison\nSpiders Poison\nGariels Feather');
+		//message.channel.send();
+
+	let leggedtofwe = new Discord.RichEmbed()
+		.setTitle('Legged to Feather Wings Egg')
+		.setImage('https://cdn.discordapp.com/attachments/502501525529034762/554647301520621599/leggedtofwe.png')
+		.setColor('#320ddb')
+		.setThumbnail('https://cdn.discordapp.com/attachments/502501525529034762/554753878462038078/unknown.png')
+		.setTimestamp()
+		//.addField('', '')
+		//.addField('', '')
+		.addField('Bahan :', '4 Leaf Clover\nAvas Wings\nFairys Powder\nMadams Feather\nCrows Feather\nChicken Feather\nRabbit Fur\nPiece of Cloth\nWhite Ash\nPlatinum\nMariels Feather\nSilver\nSnow Crystal\nPlatinum Antennae\nWhite Glove\nSilkworms Thread\nAdels Blessing\nTitanium\nFish Bone\nMithril\nMilk\nBowl\nFiber Optics');
+		//message.channel.send();
+	//Stage 3 ATK ///////////////
+
+	let bwetobabybat = new Discord.RichEmbed()
+		.setTitle('Bat Wings Egg To BabyBat/Salamander')
+		.setImage('https://cdn.discordapp.com/attachments/502501525529034762/554647579678474240/bwetobabybat.png')
+		.setColor('#320ddb')
+		.setThumbnail('https://cdn.discordapp.com/attachments/502501525529034762/554753925077794837/unknown.png')
+		.setTimestamp()
+		//.addField('', '')
+		//.addField('', '')
+		.addField('Bahan :', '4 Leaf Clover\nBats Wings\nMotherly Love\nMotherly Devotion\nMothers Heart\nFathers Excuse\nFathers Good Old Days\nSoy Milk\nMilk\nGeranium\nLarge Geranium');
+		//message.channel.send();
+
+	let bwetobloodsucking = new Discord.RichEmbed()
+		.setTitle('Bat Wings Egg to Blood Sucking/Salamander')
+		.setImage('https://cdn.discordapp.com/attachments/502501525529034762/554647540310474762/bwetobloodsucking.png')
+		.setColor('#320ddb')
+		.setThumbnail('https://cdn.discordapp.com/attachments/502501525529034762/554753925077794837/unknown.png')
+		.setTimestamp()
+		//.addField('', '')
+		//.addField('', '')
+		.addField('Bahan :', '4 Leaf Clover\nBats Wings\nVampires Canine\nSharp Tooth\nPoisonous Claws\nMushroom Poison\nSpiders Poison\nMark of Sakiel\nCrystal of Sakiel\nMark of Vampire\nTattoo\nDullahans Tear\nTooth\nFish Bone\nIvory\nBats Teeth');
+		//message.channel.send();
+
+	let bwetopigbat = new Discord.RichEmbed()
+		.setTitle('Bat Wings Egg to Pig Bat/Salamander')
+		.setImage('https://cdn.discordapp.com/attachments/502501525529034762/554647604512686092/bwetopigbat.png')
+		.setColor('#320ddb')
+		.setThumbnail('https://cdn.discordapp.com/attachments/502501525529034762/554753925077794837/unknown.png')
+		.setTimestamp()
+		//.addField('', '')
+		//.addField('', '')
+		.addField('Bahan :', '4 Leaf Clover\nBats Wings\nFarmers Scythe\nMeat\nGolden Fur\nBlack Sap\nRed Potion\nHigh-Red Potion\nMax-Red Potion\nSoy Milk\nPea\nKidney Bean\nBears Gall\nBronze Carp\nSilver Carp\nGold Carp');
+		//message.channel.send();
+
+	//Stage 3 MATK/////
+
+	let fwetopair = new Discord.RichEmbed()
+		.setTitle('Feather Wings Egg to Pair Love Bird/Eagle')
+		.setImage('https://cdn.discordapp.com/attachments/502501525529034762/554648228235313173/fwetopairlove.png')
+		.setColor('#320ddb')
+		.setThumbnail('https://cdn.discordapp.com/attachments/502501525529034762/554753970418090032/unknown.png')
+		.setTimestamp()
+		//.addField('', '')
+		//.addField('', '')
+		.addField('Bahan :', '4 Leaf Clover\nAvas Wings\nMotherly Love\nMotherly Devotion\nMothers Heart\nFathers Excuse\nFathers Good Old Days\nMans Romance\nMans Senility\nJasons Heart\nCold Heart\nHeart of Golem');
+		//message.channel.send();
+
+	let fwetocrow = new Discord.RichEmbed()
+		.setTitle('Feather Wings Egg to Crow')
+		.setImage('https://cdn.discordapp.com/attachments/502501525529034762/554648270786396162/fwetocrow.png')
+		.setColor('#320ddb')
+		.setThumbnail('https://cdn.discordapp.com/attachments/502501525529034762/554753970418090032/unknown.png')
+		.setTimestamp()
+		//.addField('', '')
+		//.addField('', '')
+		.addField('Bahan :', '4 Leaf Clover\nAvas Wings\nMark of Wind\nCrows Feather\nNecklace from Crow\nGold\nPlatinum\nSilver\nGold Dust\nMithril\nCeramic\nTitanium\nHeavy Metal\nGolemium\nCrystal\nIce Crystal\nMoonstone\nSolarstone\nShiny Thread\nRuby\nDiamond\nIlluminating Crystal\nCrystal of Star\nHeart of Wind');
+		//message.channel.send();
+
+	let fwetoowl = new Discord.RichEmbed()
+		.setTitle('Feather Wings Egg to Owl')
+		.setImage('https://cdn.discordapp.com/attachments/502501525529034762/554648248262983701/fwetoowl.png')
+		.setColor('#320ddb')
+		.setThumbnail('https://cdn.discordapp.com/attachments/502501525529034762/554753970418090032/unknown.png')
+		.setTimestamp()
+		//.addField('', '')
+		//.addField('', '')
+		.addField('Bahan :', '4 Leaf Clover\nAvas Wings\nMark of Great Sage\nMagic Powder\nSoul of Great Mage\nMark of Mage\nConjurers Robe(T)\nConjurers Robe(B)\nConjurers Hat\nConjurers Shoes\nMagic Leather Clothes(T)\nMagic Leather Clothes(B)\nMagic Leather Shoes\nMagic Leather Hat\nEnchanted Cotton Shoes\nEnchanted Cotton Hat\nEnchanted Cotton Tunic(T)\nEnchanted Cotton Tunic(B)\nEyeball');
+		//message.channel.send();
+
+	//Stage 4 ATK///
+
+	let babybatlast = new Discord.RichEmbed()
+		.setTitle('Baby Bat to HBB/Dragon')
+		.setImage('https://cdn.discordapp.com/attachments/502501525529034762/554648962901213194/babybattohbb.png')
+		.setColor('#320ddb')
+		.setThumbnail('https://cdn.discordapp.com/attachments/502501525529034762/554754016400375818/unknown.png')
+		.setTimestamp()
+		//.addField('', '')
+		//.addField('', '')
+		.addField('Bahan :', '4 Leaf Clover\nFlamberge\nFlame Dagger\nCrimson Bastard\nHot Blood\nBoiling Blood\nMans Romance\nBlue Blood of Reptile\nBats Wings\nBats Teeth\nEye Sore\nVampires Canine\nGolden Bowie');
+		//message.channel.send();
+
+	let bloodsucklast = new Discord.RichEmbed()
+		.setTitle('Blood Sucking Bat to Count Bat/Dragon')
+		.setImage('https://cdn.discordapp.com/attachments/502501525529034762/554648939006263326/bloodysuckingtocountbat.png')
+		.setColor('#320ddb')
+		.setThumbnail('https://cdn.discordapp.com/attachments/502501525529034762/554754062709686312/unknown.png')
+		.setTimestamp()
+		//.addField('', '')
+		//.addField('', '')
+		.addField('Bahan :', '4 Leaf Clover\nGlare Blade\nEbullition\nEye Sore\nVampires Canine\nMark of Vampire\nFlamberge\nFlame Dagger\nCrimson Bastard\nHot Blood\nBoiling Blood\nBats Teeth\nBlue Blood of Reptile\nGolden Bowie');
+		//message.channel.send();
+
+	let pigbatlast = new Discord.RichEmbed()
+		.setTitle('Pig Bat to Lucky Pig Bat/Dragon')
+		.setImage('https://cdn.discordapp.com/attachments/502501525529034762/554648913127407616/pigbattolpb.png')
+		.setColor('#320ddb')
+		.setThumbnail('https://cdn.discordapp.com/attachments/502501525529034762/554754253898383360/unknown.png')
+		.setTimestamp()
+		//.addField('', '')
+		//.addField('', '')
+		.addField('Bahan :', '4 Leaf Clover\nMinotaurs Passion\nCow Skin\nRabbit Ears\nChicken Costume(B)\nBah Bahs Stick\nDragons Water Goggles\nSnake Skin\nWagon Drivers Staff\nWolf Skin\nFarmers Scythe\nLucky Card\nWhite Base\nWings of Incubus\nWooden Bludgeon');
+		//message.channel.send();
+
+	let salamanderlast = new Discord.RichEmbed()
+		.setTitle('Salamander to Hacling/Dragon/Fly Dragon')
+		.setImage('https://cdn.discordapp.com/attachments/502501525529034762/554648991308972062/salamandertohaclig.png')
+		.setColor('#320ddb')
+		.setThumbnail('https://cdn.discordapp.com/attachments/502501525529034762/554754297032736790/unknown.png')
+		.setTimestamp()
+		//.addField('', '')
+		//.addField('', '')
+		.addField('Bahan :', '4 Leaf Clover\nBlue Blaze\nSnake Skin\nCow Skin\nCarp\nSilk Carp\nPoisonous Claws\nPlatinum Antennae\nBats Wings\nWings of Incubus\nHorn of Incubus\nMermaids Bubble\nCrescent Moon\nDragons Water Goggles\nSwimming Goggles\nWings of Succubus');
+		//message.channel.send();
+
+	//Stage 4 Matk////
+
+	let crowtoibis = new Discord.RichEmbed()
+		.setTitle('Crow to Sacred Ibis/Phoenix')
+		.setImage('https://cdn.discordapp.com/attachments/502501525529034762/554649359468199947/crowtoibis.png')
+		.setColor('#320ddb')
+		.setThumbnail('https://cdn.discordapp.com/attachments/502501525529034762/554754342360449024/unknown.png')
+		.setTimestamp()
+		//.addField('', '')
+		//.addField('', '')
+		.addField('Bahan :', '4 Leaf Clover\nCrystalline Dagger\nCrystalline Sword\nTranslucent Great Sword\nTransparent Mace\nCrystalline Staff\nCrystalline Shield\nMagnum Blade\nTwo-handed Magnum\nMetronicker\nPlatinum Plunger\nFalchion\nSlayer\nDivine Blade\nRadiant Great Sword\nCleric Piyas Syringe\nSoul Dagger\nAqueous Mace\nStaff of Awakening');
+		//message.channel.send();
+
+	let pairtosweet = new Discord.RichEmbed()
+		.setTitle('Pair Love Birds to Sweet Love Birds/Phoenix')
+		.setImage('https://cdn.discordapp.com/attachments/502501525529034762/554649340589899779/pairlovetosweetlove.png')
+		.setColor('#320ddb')
+		.setThumbnail('https://cdn.discordapp.com/attachments/502501525529034762/554754383452176396/unknown.png')
+		.setTimestamp()
+		//.addField('', '')
+		//.addField('', '')
+		.addField('Bahan :', '4 Leaf Clover\nRabbits Cleaver\nFarmers Scythe\nRed Plunger\nBlue Plunger\nGolden Plunger\nPlatinum Plunger\nGrizzly Cleaver\nShark Shank\nGrannys Staff\nBowl\nLucky Card\nScrubber\nMotherly Love\nFathers Good Old Days\nCool Water Kimchee\nBland Water Kimchee');
+		//message.channel.send();
+
+	let owltodrowl = new Discord.RichEmbed()
+		.setTitle('Owl to Dr Owl/Phoenix')
+		.setImage('https://cdn.discordapp.com/attachments/502501525529034762/554649317076500481/owltodrowl.png')
+		.setColor('#320ddb')
+		.setThumbnail('https://cdn.discordapp.com/attachments/502501525529034762/554754419951140876/unknown.png')
+		.setTimestamp()
+		//.addField('', '')
+		//.addField('', '')
+		.addField('Bahan :', '4 Leaf Clover\nBoots of Starlight\nArmor of Starlight(T)\nArmor of Starlight(B)\nHelmet of Starlight\nCrescent Moon\nLunar Shadow Helmet\nLunar Shadow Armor(T)\nLunar Shadow Armor(B)\nLunar Shadow Boots\nMoonstone\nShooting Star\nCrystal of Star');
+		//message.channel.send();
+
+	let eagletothunder = new Discord.RichEmbed()
+		.setTitle('Eagle to Thunder Birds/Phoenix')
+		.setImage('https://cdn.discordapp.com/attachments/502501525529034762/554649386249093121/eageltothunderbird.png')
+		.setColor('#320ddb')
+		.setThumbnail('https://cdn.discordapp.com/attachments/502501525529034762/554754475479269376/unknown.png')
+		.setTimestamp()
+		//.addField('', '')
+		//.addField('', '')
+		.addField('Bahan :', '4 Leaf Clover\nAvas Wings\nSnake Skin\nGold Carp\nHatchlings Scale\nCrustaceans Bone\nChicken Costume(H)\nFlamberge\nGolden Bowie\nCrimson Bastard\nRuby\nRed Fire\nSunlight Shield\nChicken Costume(T)');
+		//message.channel.send();
+
+	const collector = new Discord.MessageCollector(message.channel, m => m.author.id === message.author.id, {time: 10000});
+	console.log(collector)
+	collector.on('collect', message => {
+		if(message.content === '1') {
+			message.channel.send(birdeggtolegged)
+		}
+		if(message.content === '2') {
+			message.channel.send(leggedtobwe)
+			message.channel.send(leggedtofwe)
+		}
+		if(message.content === '3') {
+			message.channel.send(bwetobabybat)
+			message.channel.send(bwetobloodsucking)
+			message.channel.send(bwetopigbat)
+		}
+		if(message.content === '4') {
+			message.channel.send(fwetopairlove)
+			message.channel.send(fwetocrow)
+			message.channel.send(fwetoowl)
+		}
+		if(message.content === '5') {
+			message.channel.send(babybatlast)
+		}
+		if(message.content === '6') {
+			message.channel.send(bloodsucklast)
+		}
+		if(message.content === '7') {
+			message.channel.send(pigbatlast)
+		}
+		if(message.content === '8') {
+			message.channel.send(salamanderlast)
+		}
+		if(message.content === '9') {
+			message.channel.send(crowtoibis)
+		}
+		if(message.content === '10') {
+			message.channel.send(pairtosweet)
+		}
+		if(message.content === '11') {
+			message.channel.send(owltodrowl)
+		}
+		if(message.content === '12') {
+			message.channel.send(eagletothunder)
+		}
+		msg.delete()
+		return;
+	});
+
+}
+
+module.exports.help = {
+	name: "helpbird"
+}
